edd9421c3d975bda9e945f61b2767335f56c252c
balicz3k <baliczek_10@vp.pl>
2025-05-05 21:10:24 +0200

    feat: Enhance file selection dialog with logging for user actions

--- src/ui_bindings.rs
+++ src/ui_bindings.rs
@@ -1,4 +1,4 @@
-use crate::log_dbg;
+use crate::{log_dbg, log_inf, log_wrn, log_err};
 use crate::common::MainWindow; // Import MainWindow from common.rs
 use slint::{ComponentHandle, Weak};
 use native_dialog::{DialogBuilder};
@@ -30,17 +30,28 @@ impl UiBindings {
         let weak_ui = ui.as_weak();
         ui.on_select_file(move || {
             if let Some(ui) = weak_ui.upgrade() {
-                if let Some(path) = DialogBuilder::file()
+
+                let dialog_result = DialogBuilder::file()
                     .set_location("~/Desktop")
                     .add_filter("PDF Files", &["pdf"])
                     .add_filter("ZIP Files", &["zip"])
                     .add_filter("TAR Files", &["tar"])
                     .open_single_file()
-                    .show()
-                    .unwrap(){
-                    let path_str = path.to_string_lossy().to_string();
-                    ui.set_input_file(path_str.into());
-                    ui.set_is_file_included(true);
+                    .show();
+
+                match dialog_result {
+                    Ok(Some(path)) => {
+                        let path_str = path.to_string_lossy().to_string();
+                        log_inf!("Selected file: \"{}\"", path_str);
+                        ui.set_input_file(path_str.into());
+                        ui.set_is_file_included(true);
+                    }
+                    Ok(None) => {
+                        log_inf!("File selection cancelled.");
+                    }
+                    Err(e) => {
+                        log_err!("Error opening file dialog: {}", e);
+                    }
                 }
             }
         });
